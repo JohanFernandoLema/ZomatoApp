70e1c585b7a52f962e815e0761b44e69457fa2c9
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-01 10:29:26 -0400

    mongoose installed and and created connection to dabatase

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const AppRouter = require('./routes/AppRoutes')
 const PORT = 3000
 
 AppRouter.get('/', AppRouter)
+const MONGO_DB_URI =
+  ' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1'
 
-app.listen(PORT, () => {
-  console.log('We are on;')
-})
+mongoose
+  .connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('We are on;')
+    })
+  })
+  .catch((err) => console.log(err))
