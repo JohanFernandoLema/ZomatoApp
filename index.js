c04f5bff69f1a9e04c712468e0540ffeb31f98ff
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-02 10:04:52 -0400

    generated few user controller in order to access customer data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const MONGO_DB_URI =
   ' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1'
 
 mongoose
-  .connect()
+  .connect(MONGO_DB_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log('We are on;')
--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -4,4 +4,7 @@ const UserController = require('../controller/UserController')
 
 AppRouter.get('/', UserController.userHome)
 
+AppRouter.get('./get-user-list/:gender', UserController.getUserList)
+
+AppRouter.post('/save-user-data', UserController.getUserData)
 module.exports = AppRouter
