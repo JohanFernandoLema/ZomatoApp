ad731405ec59a8452d58bfd1bf264a746c71f73d
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-05 09:01:22 -0400

    database with restaurant data connected successfully

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const app = express()
+
+//Connect to the database
+// 1- import mongoose (install)
 const mongoose = require('mongoose')
+
 const AppRouter = require('./routes/AppRoutes')
 const PORT = 3000
 
 app.use('/api', AppRouter)
 // AppRouter.get('/get-user-list',)
-const MONGO_DB_URI =
-  ' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1'
 
-// mongoose
-//   .connect(MONGO_DB_URI)
-//   .then(() => {})
-//   .catch((err) => console.log(err))
-app.listen(PORT, () => {
-  console.log('We are on;')
-})
+// 2- use the url located in the command promp at the moment of typing mongosh
+const MONGO_DB_URI = 'mongodb://127.0.0.1:27017/restaurantData'
+
+// call mongoose with a promise
+mongoose
+  .connect(MONGO_DB_URI)
+  .then(() => {
+    console.log('DB connected successfully')
+    app.listen(PORT, () => {
+      console.log('We are on')
+    })
+  })
+  .catch((err) => console.log(err))
