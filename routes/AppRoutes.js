e6222d26878e0a128983136f43874a041a167228
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-08 10:05:30 -0400

    new API for location list added

--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -1,13 +1,15 @@
 const AppRouter = require('express').Router()
 
 const UserController = require('../controller/UserController')
+const { getLocationList } = require('../controller/LocationController')
 
 AppRouter.get('/', UserController.userHome)
 
 AppRouter.get('/get-user-list/:gender', UserController.getUserList)
 
-AppRouter.post('/save-user-data', UserController.saveUserData)
+AppRouter.get('/get-location', LocationController.getLocationList)
 
+AppRouter.post('/save-user-data', UserController.saveUserData)
 AppRouter.post('/login', UserController.userLogin)
 
 module.exports = AppRouter
