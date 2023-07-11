43078f44812f9c7b2aa4bd733835b23dca226ff9
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-11 09:10:55 -0400

    Restaurant api completed and retrieving expected results

--- controller/RestaurantController.js
+++ controller/RestaurantController.js
@@ -4,7 +4,11 @@ const RestaurantModel = require('../model/RestaurantModel')
 const RestaurantController = {
   getRestaurantListByLoc: async (req, res) => {
     let { loc_id } = req.params
-    let result = await RestaurantModel.find({ location_id: loc_id })
+    let result = await RestaurantModel.find(
+      { location_id: loc_id },
+      //Sort elements to be found only by the below mentioned parameters
+      { name: 1, locality: 1, image: 1, city: 1 }
+    )
     res.send({ status: true, result })
   },
   // getSingleRestaurantDetail: async (req, res) => {
